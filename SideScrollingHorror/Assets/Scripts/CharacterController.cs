1eef3359f1c96465b2765ed8462f5ba4a334b663
FranLenny <42728905+FranLenny@users.noreply.github.com>
2020-05-03 16:15:53 -0400

    movement with flip

---
+++ SideScrollingHorror/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+    public float speed;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+        if (facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveHorizontal <0)
+        {
+            Flip();
+        }
+    }
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+}
