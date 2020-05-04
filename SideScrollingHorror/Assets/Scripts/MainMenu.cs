e37f9f0df9ddb8fe605fce5ba91fcebcf9e5af72
FranLenny <42728905+FranLenny@users.noreply.github.com>
2020-05-03 23:10:46 -0400

    MainMenu

---
+++ SideScrollingHorror/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void startGame()
+   {
+        SceneManager.LoadScene("neverEndingRoad");
+   }
+
+    public void instructions()
+    {
+        SceneManager.LoadScene("instructions");
+    }
+    
+    public void credits()
+    {
+        SceneManager.LoadScene("credits");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
